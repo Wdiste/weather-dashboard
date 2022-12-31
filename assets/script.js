3333f568b39f194a096b39baab713d6f353e1b97
wdiste <Wdiste49@gmail.com>
2022-12-31 16:48:03 -0500

    html structure finalized.  api key established

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
 
+var lat;
+var lon;
 
-// clipped from module 6 for reference
-// fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}', {
-//   method: 'GET', //GET is the default.
-//   credentials: 'same-origin', // include, *same-origin, omit
-//   redirect: 'follow', // manual, *follow, error
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={${lat}}&lon={${lon}}&appid={6fca4038d3631546ec81e4466072f2da}`, {
+  method: 'GET', //GET is the default.
+  credentials: 'same-origin', // include, *same-origin, omit
+  redirect: 'follow', // manual, *follow, error
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
