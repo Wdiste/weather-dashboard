57c67b7a42f6fbefea80ddf106cbc338f2a34518
wdiste <Wdiste49@gmail.com>
2022-12-30 19:38:44 -0500

    barebones html, specifacally search box done.  bootstrap and styling in progress.

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+
+
+// clipped from module 6 for reference
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}', {
+//   method: 'GET', //GET is the default.
+//   credentials: 'same-origin', // include, *same-origin, omit
+//   redirect: 'follow', // manual, *follow, error
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
