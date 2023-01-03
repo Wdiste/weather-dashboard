29f46879a3a5ee196b361d43e400635e6a720136
wdiste <Wdiste49@gmail.com>
2023-01-02 19:04:19 -0500

    button list populates with unique array of searches

--- assets/script.js
+++ assets/script.js
@@ -2,30 +2,28 @@ var lat = 39;
 var lon = 106;
 
 const submit = $("#city-submit");
-const recentList = $("#recent-searches");
+const recentList = $(".recent-searches");
 var recentSearches = [];
+
+initArray();
 
 submit.on("click", getWeather);
 
 function getWeather(event) {
   event.preventDefault();
 
-  // initialize saved searches array
-  if (localStorage.getItem("recentSearches") == null) {
-    console.log("returned null");
-    localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
-  } else {
-    recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
-  }
-
-
-  console.log('recent searches = ' + recentSearches);
-
   let input = $("#input-city");
+
   // add recent search to list, save to local data
-  console.log(input.val());
   recentSearches.push(input.val());
-  localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+
+  // remove duplicate searches and save array
+  localStorage.setItem(
+    "recentSearches",
+    JSON.stringify(cleanArray(recentSearches))
+  );
+
+  initArray();
 
   // fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={${lat}}&lon={${lon}}&appid={0bab6c4bf9594e7a0fa86f4f8b8f741f}`, {
   // method: 'GET', //GET is the default.
@@ -38,4 +36,30 @@ function getWeather(event) {
   // .then(function (data) {
   //     console.log(data);
   // });
+}
+
+function initArray() {
+  // initialize saved searches array
+  if (localStorage.getItem("recentSearches") == null) {
+    localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+  } else {
+    // if data exists, pull it from saveds
+    recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
+
+    // clean up list to prevent duplicates
+    recentList.empty();
+    for (i = 0; i < recentSearches.length; i++) {
+      recentList.append(
+        $("<li>").append(
+          $(
+            `<button type="button" class="btn btn-secondary">${recentSearches[i]}</button>`
+          ).click(getWeather)
+        )
+      );
+    };
+  };
+};
+
+function cleanArray(array) {
+  return array.filter((item, index) => array.indexOf(item) === index);
 }
