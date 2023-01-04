828e14b96bb6021959fbba296e1161310b3cfa5e
wdiste <Wdiste49@gmail.com>
2023-01-04 16:53:23 -0500

    weather cards icon working now.  changed weather to forecast to get all 5 days working

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,7 @@ function getWeather(event) {
   initArray();
 
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${input.val()}&appid=54da9036a4b40ba467ebb59246c77917`,
+    `https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=${input.val()}&appid=54da9036a4b40ba467ebb59246c77917`,
     {
       method: "GET", //GET is the default.
       credentials: "same-origin", // include, *same-origin, omit
@@ -33,20 +33,20 @@ function getWeather(event) {
     .then(function (data) {
       // ~~~ for date conversion.  data.dt is unix code, must be converted.
       // ~~~ add data.timezone to data.dt; multiply by 1000 for date in seconds
-      let date = new Date((data.dt + data.timezone) * 1000 + 18000000);
+      let date = new Date(data.list.dt);
       // having issue with getting accurate times.  need to add 5 hrs to the given time
       // from openweathermap api
-
+      // + data.list.timezone) * 1000 + 18000000
 
       // setting up a template literal for card creation
       const weatherCardTemplate = 
       ` <li class="card" style="width:19.5%;display:inline-block;">
-            <div class="card-header">${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}</div>
-            <ul class="list-group list-group-flush card-list style="float:left;display:inline;"">
-              <li class="list-group-item"><img id="wicon" src="${data.weather[0].icon}" alt="Weather icon"></li>
-              <li class="list-group-item">Temp: ${data.main.temp}°F</li>
-              <li class="list-group-item">Wind: ${data.wind.speed}MPH</li>
-              <li class="list-group-item">Humidity: ${data.main.humidity}%</li>
+            <div class="card-header">${data.list[0].dt_txt}</div>
+            <ul class="list-group list-group-flush card-list style="display:inline;"">
+              <li class="list-group-item"><img id="wicon" src="https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png" alt="Weather icon"></li>
+              <li class="list-group-item">Temp: ${data.list[0].main.temp}°F</li>
+              <li class="list-group-item">Wind: ${data.list[0].wind.speed}MPH</li>
+              <li class="list-group-item">Humidity: ${data.list[0].main.humidity}%</li>
             </ul>
         </li>`;
       
@@ -56,15 +56,12 @@ function getWeather(event) {
       }
 
       console.log(data);
-      console.log("Name: " + data.name); // city name,
-      console.log(`Date: ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} : ${date.getHours()}`); // date
-      console.log("Icon: " + data.weather[0].icon); // weather icon
-      console.log("Temp: " + data.main.temp); // temp
-      console.log("Wind: " + data.wind.speed); // wind
-      console.log(`Humidity: ${data.main.humidity}`); // humidity
-      console.log("timezone = " + data.timezone); // same for next 5 days
-
-      console.log(weatherCardTemplate);
+      console.log("Name: " + input.val()); // city name,
+      console.log("date: " + data.list[0].dt_txt); // date
+      console.log("Icon: " + data.list[0].weather[0].icon); // weather icon
+      console.log("Temp: " + data.list[0].main.temp); // temp
+      console.log("Wind: " + data.list[0].wind.speed); // wind
+      console.log(`Humidity: ${data.list[0].main.humidity}`); // humidity
     });
 }
 
