201766ff9868c771ee2269cfa5d11579d6a44b74
wdiste <Wdiste49@gmail.com>
2023-01-04 16:33:56 -0500

    weather cards properly formatted and display dynamically

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 const submit = $("#city-submit");
 const recentList = $(".recent-searches");
+const weatherCards = $(".weather-cards")
 var recentSearches = [];
 
 initArray();
@@ -14,36 +15,57 @@ function getWeather(event) {
   recentSearches.push(input.val());
 
   // remove duplicate searches and save array
-  localStorage.setItem(
-    "recentSearches",
-    JSON.stringify(cleanArray(recentSearches))
-  );
+  localStorage.setItem("recentSearches",JSON.stringify(cleanArray(recentSearches)));
   // update recent searches button list:
   initArray();
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${input.val()}&appid=54da9036a4b40ba467ebb59246c77917`, {  
-  method: 'GET', //GET is the default.
-  credentials: 'same-origin', // include, *same-origin, omit
-  redirect: 'follow', // manual, *follow, error
-  })
-  .then(function (response) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${input.val()}&appid=54da9036a4b40ba467ebb59246c77917`,
+    {
+      method: "GET", //GET is the default.
+      credentials: "same-origin", // include, *same-origin, omit
+      redirect: "follow", // manual, *follow, error
+    }
+  )
+    .then(function (response) {
       return response.json();
-  })
-  .then(function (data) {
-      let date = new Date(((data.dt + data.timezone) * 1000) + 18000000); 
-      // having issue with getting accurate times.  need to add 5 hrs to the given time 
-      // from openweathermap api
-      console.log(data);
-      console.log('Name: ' + data.name);// city name, 
+    })
+    .then(function (data) {
       // ~~~ for date conversion.  data.dt is unix code, must be converted.
       // ~~~ add data.timezone to data.dt; multiply by 1000 for date in seconds
-      console.log(`Date: ${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()} : ${date.getHours()}`); // date 
-      console.log('Icon: ' + data.weather[0].icon);// weather icon
-      console.log('Temp: ' + data.main.temp);// temp
-      console.log('Wind: ' + data.wind.speed);// wind
-      console.log(`Humidity: ${data.main.humidity}`);// humidity
-      console.log('timezone = ' + data.timezone);// same for next 5 days
-  });
+      let date = new Date((data.dt + data.timezone) * 1000 + 18000000);
+      // having issue with getting accurate times.  need to add 5 hrs to the given time
+      // from openweathermap api
+
+
+      // setting up a template literal for card creation
+      const weatherCardTemplate = 
+      ` <li class="card" style="width:19.5%;display:inline-block;">
+            <div class="card-header">${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}</div>
+            <ul class="list-group list-group-flush card-list style="float:left;display:inline;"">
+              <li class="list-group-item"><img id="wicon" src="${data.weather[0].icon}" alt="Weather icon"></li>
+              <li class="list-group-item">Temp: ${data.main.temp}°F</li>
+              <li class="list-group-item">Wind: ${data.wind.speed}MPH</li>
+              <li class="list-group-item">Humidity: ${data.main.humidity}%</li>
+            </ul>
+        </li>`;
+      
+      weatherCards.empty();
+      for(i = 0; i < 5; i++) {
+        weatherCards.append(weatherCardTemplate);
+      }
+
+      console.log(data);
+      console.log("Name: " + data.name); // city name,
+      console.log(`Date: ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} : ${date.getHours()}`); // date
+      console.log("Icon: " + data.weather[0].icon); // weather icon
+      console.log("Temp: " + data.main.temp); // temp
+      console.log("Wind: " + data.wind.speed); // wind
+      console.log(`Humidity: ${data.main.humidity}`); // humidity
+      console.log("timezone = " + data.timezone); // same for next 5 days
+
+      console.log(weatherCardTemplate);
+    });
 }
 
 function initArray() {
@@ -64,15 +86,11 @@ function initArray() {
           ).click(getWeather)
         )
       );
-    };
-  };
-};
-
-function cleanArray(array) {
-    // remove all duplicate elements
-  return array.filter((item, index) => array.indexOf(item) === index);
+    }
+  }
 }
 
-// function getDate(uDate) {
-//   new Date(obj.dt*1000+(obj.timezone*1000))
-// }
+function cleanArray(array) {
+  // remove all duplicate elements
+  return array.filter((item, index) => array.indexOf(item) === index);
+}
